0f47800d742179208b532de96ba6d5d0a662e03e
Dileep <dileepcapg@rediffmail.com>
2020-12-27 22:37:27 +0530

    new java

---
+++ DemoMultiplication.java
@@ -0,0 +1,21 @@
+package com.simplileran.learning.introduction.programs;
+
+public class DemoMultiplication {
+
+
+public static void main(String[] args) {
+	double x = 4.1;
+	double y = 4.12;
+	double z = x*y;
+
+double a6=100.67;
+
+int a7=(int) a6;
+
+
+
+//System.out.println("the product of" + " " +x + " " + "and" + " " +y  +" "  + "is" + " " +z);
+
+System.out.println(a7);
+}
+}
---
+++ DemoProgram.java
@@ -0,0 +1,13 @@
+package com.simplileran.learning.introduction.programs;
+
+public class DemoProgram {
+
+	public static void main(String[] args) {
+		int a=1;
+		int b=7;
+		int c=a+b;
+		
+		System.out.println("sum of" +" " +a +" "+ "and");
+
+	}
+}
